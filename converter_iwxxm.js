dbffdab6dc5e8174f9b7d93b12f97d4744026ed4
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-03-06 16:54:38 -0500

    Update converter_iwxxm.js

--- converter_iwxxm.js
+++ converter_iwxxm.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var util = require('util');
-var xml4js = require('xml4js');
+var xml4js = require('./xml4js');
 var xml2js = require('xml2js');
 
 
@@ -10,28 +10,21 @@ var parser = new xml4js.Parser(options);
 
 
 // Default is not to download schemas automatically, so we should add it manually
-var xsd = fs.readFileSync('./tests/xsd/IWXXM/taf.xsd', {encoding: 'utf-8'});
-var xml = fs.readFileSync('./tests/xml/IWXXM/taf-A5-2.xml', {encoding: 'utf-8'});
+var xsd = fs.readFileSync('./tests/xsd/IWXXM/airmet.xsd', {encoding: 'utf-8'});
+var xml = fs.readFileSync('./tests/xml/IWXXM/airmet-A6-1a-TS.xml', {encoding: 'utf-8'});
 
-//TODO: metarSpeci is not working... 
-//var xsd = fs.readFileSync('./tests/xsd/IWXXM/metarSpeci.xsd', {encoding: 'utf-8'});
-//var xml = fs.readFileSync('./tests/xml/IWXXM/metar-A3-1.xml', {encoding: 'utf-8'});
-
+var parseString = require('xml2js').parseString;
 
 parser.addSchema('http://icao.int/iwxxm/3.0', xsd, function (err, importsAndIncludes) { 
-    //importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in xsd file
-    //convert XML to JSON
-    xml2js.parseString(xml, { mergeAttrs: true }, (err, result) => {
-        if (err) {
-            throw err;
-    };
+    // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in xsd file
+    
+    //convert xml to json
+    parseString(xml, function (err, result) {
+        //var json = JSON.stringify(result, null, 4);
+        console.log(util.inspect(result, false, null));
+        console.dir(result.Ffice2Message);
+        var json = JSON.stringify(result, null, 2);
 
-    //convert it to a JSON string
-    const json = JSON.stringify(result, null, 4);
-    console.log(util.inspect(result, false, null));
-
-    //save JSON in a file
-    //TODO: saving as JSON file hasn't worked so far
-    //fs.writeFileSync('./tests/outcome_iwxxm/taf-A5-2.json', json);
+        fs.writeFileSync('./tests/output_iwxxm/airmet-A6-1a-TS.json', json);
     });
 });
