2defd93d6bae3e6df762ef9bf4655ba80e666d4f
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-04-13 12:24:37 -0400

    Update converter_iwxxm.js

--- converter_iwxxm.js
+++ converter_iwxxm.js
@@ -13,10 +13,10 @@ var xsd = fs.readFileSync('./tests/xsd/IWXXM/airmet.xsd', {encoding: 'utf-8'});
 var xml = fs.readFileSync('./tests/xml/IWXXM/airmet-translation-failed.xml', {encoding: 'utf-8'});
 
 
-parser.addSchema('http://icao.int/iwxxm/3.0', xsd, function (err, importsAndIncludes) { 
+parser.addSchema('http://icao.int/iwxxm/3.0', xsd, (err, importsAndIncludes) => { 
     //importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in xsd file
     //converting
-    parseString(xml, function (err, result) {
+    parseString(xml, (err, result) => {
         console.log(util.inspect(result, false, null)); //displays the whole result
         console.dir(result);
         var json = JSON.stringify(result, null, 2);
